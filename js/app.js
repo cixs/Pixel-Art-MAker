e29a0f0ad16f8361bee632d31b25bae0d506384f
Cristinel Ilisei <cristinel.ilisei@gmail.com>
2018-03-08 07:09:33 +0200

    chore:  Replace the default colorpicker with the fancy farbtastic colorpicker (created by Steven Wittens)
    It does need to keep the input element inside
    since the user dont't have to pick the color code.
    Instead, I created a rectangle to display the chosen color as a preview.

--- js/app.js
+++ js/app.js
@@ -1,68 +1,69 @@
-// Select color input
-// Select size input
-// When size is submitted by the user, call makeGrid()
-
 $(document).ready(function () {
 
-    const designCanvas = $("#pixelCanvas");
+  const designCanvas = $("#pixel-canvas");
+  let  color, gridHeight, gridWidth;
 
-    let  color, gridHeight, gridWidth;
+  color = $(".color-box");
+  gridHeight = $("#input-height");
+  gridWidth = $("#input-width");
 
-    color = $("input[type='color']");
-    gridHeight = $("#inputHeight");
-    gridWidth = $("#inputWidth");
+  //make grid
+  function makeGrid() {
 
-    //make grid
-    function makeGrid() {
+      designCanvas.html("");
+      designCanvas.css("background-color", document.body.style.backgroundColor);
 
-        $(".helper").css("display", "none");
-        $(".canvas-title").html("Canvas design");
+      //make markup for a row in the table
+      let strRow = "<tr>";
+      for (let i = 0; i < gridWidth.val(); i++) {
+          strRow += "<td></td>";
+      }
+      strRow += "</tr>";
 
-        designCanvas.html("");
-        designCanvas.css("background-color", document.body.style.backgroundColor);
+      //append #gridHeight rows in the table
+      for (let j = 0; j < gridHeight.val(); j++) {
+          designCanvas.append(strRow);
+      }
+  }
 
-        //make markup for a row in the table
-        let strRow = "<tr>";
-        for (let i = 0; i < gridWidth.val(); i++) {
-            strRow += "<td></td>";
-        }
-        strRow += "</tr>";
+  //handler for submit button mouseclick
+  $("form").submit(function (e) {
+      e.preventDefault();
+      $(".helper").css("display", "none");
+      $(".canvas-title").css("display", "block");
 
-        //append gridHeight rows in the table
-        for (let j = 0; j < gridHeight.val(); j++) {
-            designCanvas.append(strRow);
-        }
-    }
+      makeGrid();
 
-    //handler for submit button mouseclick
-    $("form").submit(function (e) {
-        e.preventDefault();
+      //calculate cellSize based on the grid width
+      let cellSize = document.body.clientWidth / gridWidth.val();
+      if (cellSize > 20)
+          cellSize = 20; //prevent drawing large cells if a small sized grid was chosen
 
-        makeGrid();
+      $("tr").css("height", cellSize);
+      $("td").css("width", cellSize);
 
-        //calculate cellSize based on the grid width
-        let cellSize = document.body.clientWidth / gridWidth.val();
-        if (cellSize > 20)
-            cellSize = 20; //prevent drawing large cells if a small sized grid was chosen
+      //prevent shrinking cells when window is resized
+      designCanvas.css("width", cellSize*gridWidth.val());
 
-        $("tr").css("height", cellSize);
-        $("td").css("width", cellSize);
+  });
 
-        //prevent shrinking cells when window is resized
-        designCanvas.css("width", cellSize*gridWidth.val());
+  //change cell background color when a mouse left button is clicked
+  designCanvas.on("mousedown", "td", function (evt) {
+      $(evt.target).css("background-color", color.css("backgroundColor"));
+      console.log(color, color.css("backgroundColor"));
+  });
 
-    });
+  //continuously drawing by moving the mouse
+  designCanvas.on("mouseover", "td", function (evt) {
+      evt.preventDefault();
+      if (evt.buttons === 1) {
+          $(this).css("background-color", color.css("backgroundColor"));
+      }
+  });
 
-    //change cell background color when a mouse left button is clicked
-    designCanvas.on("mousedown", "td", function (evt) {
-        $(evt.target).css("background-color", color.val());
-    });
-
-    //continuously drawing by moving the mouse
-    designCanvas.on("mouseover", "td", function (evt) {
-        evt.preventDefault();
-        if (evt.buttons === 1) {
-            $(this).css("background-color", color.val());
-        }
-    });
+  var picker = $.farbtastic("#colorpicker");
+  picker.setColor("#fff");
+  picker.linkTo(function onColorChange(colorValue) {
+    color.css("background-color", colorValue);
+  });
 });
